2c947730d68e516f9ed6c2b4c766d88964838ba0
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 16:25:17 -0600

    Project consumer for Rabbit

---
+++ Ecommerce.ConsoleApp.Consumer/Ecommerce.ConsoleApp.Consumer/DiscountCreatedConsumer.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Domain.Events;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Ecommerce.ConsoleApp.Consumer
+{
+    internal class DiscountCreatedConsumer : IConsumer<DiscountCreatedEvent>
+    {
+        public async Task Consume(ConsumeContext<DiscountCreatedEvent> context)
+        {
+            var jsonMessage = JsonSerializer.Serialize(context.Message);
+            await Console.Out.WriteLineAsync($"Mensaje from producer : {jsonMessage}");
+        }
+    }
+}
---
+++ Ecommerce.ConsoleApp.Consumer/Ecommerce.ConsoleApp.Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ecommerce.ConsoleApp.Consumer;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+public class Program
+{
+    public async static Task Main(string[] args)
+    {
+        await  Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddMassTransit(x=>
+                {
+                    x.AddConsumer<DiscountCreatedConsumer>();
+                    x.UsingRabbitMq((context, cfg) =>
+                    {
+                        cfg.Host("localhost", "/", h =>
+                        {
+                            h.Username("guest");
+                            h.Password("guest");
+                        });
+
+                        cfg.ConfigureEndpoints(context);
+                    });
+                });  
+            })
+            .Build()
+            .RunAsync();
+    }
+}
+
+
